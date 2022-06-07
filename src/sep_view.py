b959205ec544a8c12d0e38a9f802e6468410c4bd
joycelin1123 <84490877+joycelin1123@users.noreply.github.com>
2022-06-07 12:23:54 -0400

    Update sep_view.py

--- src/sep_view.py
+++ src/sep_view.py
@@ -3,7 +3,7 @@
 # Required to run sep_bkg_rms.py first to obtain the bkgL and rmsL
 
 # get the fits info using the relative path of the FITS file (CHANGE THIS)
-filename = "hst_10325_c7_acs_wfc_f475w_drz.fits"
+filename = "data/hst_10325_c7_acs_wfc_f475w_drz.fits"
 
 # write down the image names in order (CHANGE THIS)
 imL = ["Image 1", "Image 2", "Image 3", "Image 4", "Image 5"]
